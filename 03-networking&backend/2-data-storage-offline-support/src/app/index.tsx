ef6a614a78ec9784564e91944ec84fa779e7aaa3
DEVESH-001 <yadavdevesh990@gmail.com>
2026-05-26 12:05:43 +0530

    Update: Expo File-System

--- 03-networking&backend/2-data-storage-offline-support/src/app/index.tsx
+++ 03-networking&backend/2-data-storage-offline-support/src/app/index.tsx
@@ -192,68 +192,261 @@
 // });
 
 // SQlite [https://docs.expo.dev/versions/latest/sdk/sqlite/]
-import * as SQLite from "expo-sqlite";
-import React, { useEffect, useState } from "react";
+// import * as SQLite from "expo-sqlite";
+// import React, { useEffect, useState } from "react";
+// import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// const db = SQLite.openDatabaseSync("demo.db");
+
+// const index = () => {
+//   const [output, setOutput] = useState("");
+
+//   const createTable = () => {
+//     db.execSync(`
+//         CREATE TABLE IF NOT EXISTS users (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         name TEXT,
+//         age INTEGER
+//       );
+
+//       `);
+
+//     setOutput("Table created");
+//   };
+
+//   const insertUser = () => {
+//     db.runSync("INSERT INTO users (name , age) VALUES (? , ?)", "Suraj", 22);
+//   };
+
+//   const getUsers = () => {
+//     const users = db.getAllSync("SELECT * FROM users");
+
+//     setOutput(JSON.stringify(users, null, 2));
+//   };
+
+//   const getFirstUser = () => {
+//     const user = db.getFirstSync("SELECT * FROM users");
+
+//     setOutput(JSON.stringify(user, null, 2));
+//   };
+
+//   const updateUser = () => {
+//     db.runSync("UPDATE users SET age = ? WHERE id = ?", 25, 1);
+
+//     setOutput("User Updated");
+//   };
+
+//   const deleteUser = () => {
+//     db.runSync("DELETE FROM users WHERE id = ?", 1);
+
+//     setOutput("User Deleted");
+//   };
+
+//   const dropTable = () => {
+//     db.execSync(`
+//       DROP TABLE IF EXISTS users;
+//     `);
+
+//     setOutput("Table Dropped");
+//   };
+
+//   useEffect(() => {
+//     createTable();
+//   }, []);
+
+//   return (
+//     <SafeAreaView
+//       style={{
+//         flex: 1,
+//       }}
+//     >
+//       <ScrollView
+//         contentContainerStyle={{
+//           padding: 20,
+//           gap: 12,
+//         }}
+//       >
+//         <Text
+//           style={{
+//             fontSize: 28,
+//             fontWeight: "bold",
+//             marginBottom: 10,
+//           }}
+//         >
+//           SQLite Demo
+//         </Text>
+
+//         <Button title="Create Table" onPress={createTable} />
+
+//         <Button title="Insert User" onPress={insertUser} />
+
+//         <Button title="Get All Users" onPress={getUsers} />
+
+//         <Button title="Get First User" onPress={getFirstUser} />
+
+//         <Button title="Update User" onPress={updateUser} />
+
+//         <Button title="Delete User" onPress={deleteUser} />
+
+//         <Button title="Drop Table" onPress={dropTable} />
+
+//         <View
+//           style={{
+//             marginTop: 20,
+//             padding: 16,
+//             borderWidth: 1,
+//             borderRadius: 10,
+//           }}
+//         >
+//           <Text
+//             style={{
+//               fontSize: 18,
+//               fontWeight: "bold",
+//               marginBottom: 10,
+//             }}
+//           >
+//             Output
+//           </Text>
+
+//           <Text
+//             selectable
+//             style={{
+//               fontSize: 14,
+//             }}
+//           >
+//             {output}
+//           </Text>
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default index;
+
+// const styles = StyleSheet.create({});
+
+// Expo File System [https://docs.expo.dev/versions/latest/sdk/filesystem/]
 import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { File, Paths, Directory } from "expo-file-system";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
+import { Image } from "expo-image";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const db = SQLite.openDatabaseSync("demo.db"); 
+export default function Index() {
+  const [output, setOutput] = useState<string>("");
+  const [downloadedImageUri, setDownloadedImageUri] = useState<string | null>(
+    null,
+  );
 
-const index = () => {
-  const [output, setOutput] = useState("");
+  // const file = new File(Paths.cache, "demo.txt"); // this creates a file object in the cache directory
+  // console.log(file);
 
-  const createTable = () => {
-    db.execSync(`
-        CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        age INTEGER
-      );
-      
-      `);
+  // create file
+  const demoFile = new File(Paths.document, "demo.txt");
+  //console.log(Paths.document);
 
-    setOutput("Table created");
+  const writeFile = async () => {
+    demoFile.write("Hello Expo File System");
   };
 
-  const insertUser = () => {
-    db.runSync("INSERT INTO users (name , age) VALUES (? , ?)", "Suraj", 22);
+  const readFile = async () => {
+    const data = await demoFile.text();
+    setOutput(data);
+    return data;
   };
 
-  const getUsers = () => {
-    const users = db.getAllSync("SELECT * FROM users");
+  // copy file
+  const copiedFile = new File(Paths.document, "demo_copy.txt");
+  //console.log("COPIED__FILE : : ", copiedFile);
 
-    setOutput(JSON.stringify(users, null, 2));
+  const copyFile = () => {
+    demoFile.copy(copiedFile);
+
+    setOutput("File copied successfully");
   };
 
-  const getFirstUser = () => {
-    const user = db.getFirstSync("SELECT * FROM users");
+  const movedFile = new File(Paths.document, "moved-demo.txt");
 
-    setOutput(JSON.stringify(user, null, 2));
+  // append -> add content to the end of the file
+  const appendFile = async () => {
+    const oldData = await readFile(); // get the existing content
+    // now append new content to the existing content
+    demoFile.write(oldData + "\nNew content");
   };
 
-  const updateUser = () => {
-    db.runSync("UPDATE users SET age = ? WHERE id = ?", 25, 1);
-
-    setOutput("User Updated");
+  const moveFile = async () => {
+    copiedFile.move(movedFile);
   };
 
-  const deleteUser = () => {
-    db.runSync("DELETE FROM users WHERE id = ?", 1);
-
-    setOutput("User Deleted");
+  const deleteAllFile = () => {
+    demoFile.delete();
+    copiedFile.delete();
+    movedFile.delete();
+    setOutput("File deleted ✅");
   };
 
-  const dropTable = () => {
-    db.execSync(`
-      DROP TABLE IF EXISTS users;
-    `);
-
-    setOutput("Table Dropped");
+  const getFileInfo = () => {
+    const info = {
+      exists: demoFile.exists,
+      size: demoFile.size,
+      uri: demoFile.uri,
+      name: demoFile.name,
+    };
+    setOutput(JSON.stringify(info, null, 2));
   };
 
-  useEffect(() => {
-    createTable();
-  }, []);
+  //      ~~~ CREATING DIRECTORY ~~~
+
+  const notesDirectory = new Directory(Paths.document, "notes");
+  //console.log("NOTES DIRECTORY : : ", notesDirectory);
+
+  //     ~~ CREATING/READING FOLDER IN DIRECTORY ~~
+  const createFolder = () => {
+    notesDirectory.create();
+  };
+  const readFolderDir = () => {
+    const files = notesDirectory.list();
+  };
+
+  const readDir = () => {
+    const files = notesDirectory.list();
+    setOutput(
+      JSON.stringify(
+        files.map((f) => f.uri),
+        null,
+        2,
+      ),
+    );
+  };
+
+  //      ~~~ DOWNLOADING ~~~
+
+  const downloadFile = async () => {
+    const folder = new Directory(Paths.cache, "images");
+    folder.create();
+
+    const downloadedFile = await File.downloadFileAsync(
+      "https://picsum.photos/300",
+      folder,
+    );
+
+    setDownloadedImageUri(downloadedFile.uri);
+
+    setOutput(
+      JSON.stringify(
+        {
+          uri: downloadedFile.uri,
+          exists: downloadedFile.exists,
+          size: downloadedFile.size,
+        },
+        null,
+        2,
+      ),
+    );
+  };
 
   return (
     <SafeAreaView
@@ -274,22 +467,19 @@ const index = () => {
             marginBottom: 10,
           }}
         >
-          SQLite Demo
+          Expo FileSystem Modern API
         </Text>
 
-        <Button title="Create Table" onPress={createTable} />
-
-        <Button title="Insert User" onPress={insertUser} />
-
-        <Button title="Get All Users" onPress={getUsers} />
-
-        <Button title="Get First User" onPress={getFirstUser} />
-
-        <Button title="Update User" onPress={updateUser} />
-
-        <Button title="Delete User" onPress={deleteUser} />
-
-        <Button title="Drop Table" onPress={dropTable} />
+        <Button title="1. Write File" onPress={writeFile} />
+        <Button title="2. Read File" onPress={readFile} />
+        <Button title="3. Append File" onPress={appendFile} />
+        <Button title="4. Get File Info" onPress={getFileInfo} />
+        <Button title="5. Copy File" onPress={copyFile} />
+        <Button title="6. Move File" onPress={moveFile} />
+        <Button title="7. Create Folder" onPress={createFolder} />
+        <Button title="8. Read Directory" onPress={readDir} />
+        <Button title="9. Download File" onPress={downloadFile} />
+        <Button title="10. Delete All Files" onPress={deleteAllFile} />
 
         <View
           style={{
@@ -309,20 +499,65 @@ const index = () => {
             Output
           </Text>
 
-          <Text
-            selectable
+          <Text selectable>{output}</Text>
+        </View>
+
+        {downloadedImageUri ? (
+          <View
             style={{
-              fontSize: 14,
+              marginTop: 20,
+              alignItems: "center",
+              gap: 8,
             }}
           >
-            {output}
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Downloaded Image
+            </Text>
+
+            <Image
+              source={{ uri: downloadedImageUri }}
+              style={{
+                width: 300,
+                height: 300,
+                borderRadius: 10,
+              }}
+              contentFit="cover"
+            />
+          </View>
+        ) : null}
+
+        <View
+          style={{
+            marginTop: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+            }}
+          >
+            Paths.document
           </Text>
+
+          <Text selectable>{Paths.document.uri}</Text>
+
+          <Text
+            style={{
+              fontWeight: "bold",
+              marginTop: 10,
+            }}
+          >
+            Paths.cache
+          </Text>
+
+          <Text selectable>{Paths.cache.uri}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-};
-
-export default index;
-
-const styles = StyleSheet.create({});
+}
